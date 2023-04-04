a96211b4686addae2e9323f5663aa5ae19070958
slurpeesz <khtbee@gmail.com>
2023-04-04 16:16:47 -0700

    added routes and handlebars

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const homeRoutes = require('./api/homeRoutes');
+router.use('/', homeRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,36 +1,43 @@
-const path = require('path');
 const express = require('express')
 const session = require('express-session')
-const exphbs = require('express-handlebars')
-const hbs= exphbs.create({ helpers })
-
 const app = express();
+const exphbs = require('express-handlebars')
+const hbs= exphbs.create()
+const routes = require("./controllers")
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/config')
+const sequelize = require('./config/connection')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-count sess = {
+const sess = {
   secret: "Super secret secret",
   cookie: {},
   resave: false,
   saveUninitialzied: true,
-  store new SequelizeStore({
+  store: new SequelizeStore({
     db: sequelize
-  })
-}
+  }),
+};
 
 app.use(session(sess));
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
 app.use(express.json());
-app.use(express.urlencoded({ extended; false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'))
+// app.use(require('./controllers'))
 
-app.listen(PORT, () => {
-  console.log (`Listening App on port ${PORT}`);
-  sequelize.sync({ force: false});
+// app.listen(PORT, () => {
+//   console.log (`Listening App on port ${PORT}`);
+//   sequelize.sync({ force: false});
+// });
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Listening"));
 });
